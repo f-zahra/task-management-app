1bd5437433b5c5f7f3fd40a0275b970fffcab9ed
f-zahra <eva-200@hotmail.fr>
2024-11-11 17:14:21 -0500

    implemented express-async-handler middleware #3

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,34 +1,47 @@
 var express = require("express");
 const taskModel = require("../models/Task");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
 /* GET all tasks */
-router.get("/:userId/", async (req, res) => {
-  try {
+router.get(
+  "/:userId/",
+  asyncHandler(async (req, res, next) => {
     const tasks = await taskModel.find({ user: req.params.userId });
 
     res.status(200).send(tasks);
-  } catch (err) {
-    res.status(404).send("not found");
-  }
-});
+  })
+);
 
 /* Get a task */
-router.get("/:userId/:taskId", function (req, res, next) {
-  //TODO
-});
+router.get(
+  "/:userId/:taskId",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 
 /*POST new task */
-router.post("/:userId/create", (req, res) => {
-  //TODO
-});
+router.post(
+  "/:userId/create",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
+
 /*UPDATE task*/
-router.put("/:userId/:taskId/update", (req, res) => {
-  //TODO
-});
+router.put(
+  "/:userId/:taskId/update",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 
 /*DELETE task */
-router.delete("/:userId/:taskId/delete", (req, res) => {
-  //TODO
-});
+router.delete(
+  "/:userId/:taskId/delete",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,45 @@
 var express = require("express");
 const userModel = require("../models/User");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
 /* GET all users */
-router.get("/", async (req, res) => {
-  try {
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
     const users = await userModel.find();
     res.status(200).send(users);
-  } catch (err) {
-    res.status(404).send("not found");
-  }
-});
+  })
+);
 
 /* Get a user */
-router.get("/:userId", function (req, res, next) {
-  //TODO
-});
+router.get(
+  "/:userId",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 
 /*POST new user */
-router.post("/create", (req, res) => {
-  //TODO
-});
+router.post(
+  "/create",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 /*UPDATE user*/
-router.put("/:userId/update", (req, res) => {
-  //TODO
-});
+router.put(
+  "/:userId/update",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 
 /*DELETE user */
-router.delete("/:userId/delete", (req, res) => {
-  //TODO
-});
+router.delete(
+  "/:userId/delete",
+  asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED");
+  })
+);
 module.exports = router;
